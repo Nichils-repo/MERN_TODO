3037ed1d4c49f2a93872bdb20416773b3ea20296
Nichil <hementhnichilg@gmail.com>
2025-06-13 19:21:24 +0530

    nodemon and start in package.json
    what is dev dependencies and who nodemon is useful and alternative run methods when we are no longer in production

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,11 @@ const app = express();
 // the req,res => {} is just a normal js function
 //which is then enclosed in the app.get which is an express method
 app.get("/api/notes", (req, res) => {
-  res.send("you got 5 notes");
+  res.status(200).send("you got 20 notes");
+});
+
+app.post("api/notes", (req, res) => {
+  res.status(201).send("note has been created successfully");
 });
 
 app.listen(5001, () => {
